3bb421bb7ab72e0e18611fda2817cd71d19ea26b
Ahsan Javaid <124583633+ehsanjavaid@users.noreply.github.com>
2025-07-23 16:18:22 +0500

    about me section done

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -34,8 +34,8 @@ const HeroText: React.FC = () => {
     }, [charIndex, isDeleting, textIndex, texts]);
 
     return (
-        <div className="pt-[130px] ml-[49.33%] bg-[#373b40] text-white flex flex-col justify-center px-10">
-            <h1 className="text-6xl font-bold leading-tight mt-[40px] ml-[-12vw] relative bg-[#373b40] p-[4.2vw] text-[4.2vw] rounded-[264px] ">Ahsan<br />Javaid</h1>
+        <div className=" pt-[130px] ml-[49.30%] bg-[#373b40] text-white flex flex-col justify-center px-[40px] flex-1">
+            <h1 className="text-6xl font-bold leading-tight mt-[40px] ml-[-16vw] relative bg-[#373b40] p-[4.2vw] text-[4.2vw] rounded-[264px] ">Ahsan<br />Javaid</h1>
             <p className="text-teal-400 mt-4 font-semibold h-8">
                 {displayedText}
                 <span className="border-r-2 border-teal-400 animate-pulse ml-1"></span>
@@ -50,6 +50,26 @@ const HeroText: React.FC = () => {
             </button>
             <div className="w-full flex justify-center">
                 <hr className="my-10 w-[100%] border-t border-gray-600" />
+            </div>
+            <div className="flex flex-col titles">
+                <div className="title text-[55px] text-white font-bold">About Me </div>
+                <div className="subtitle text-[14px] text-[#68e0cf] font-bold">MY STORY </div>
+                <div className="flex flex-row mt-[30px] justify-between  w-full mb-[30px]">
+                    <div className="pr-[20px] text-[14px] w-[50%]">I'm Ahsan Javaid, a dedicated Full Stack
+                        & ERPNext Developer based in Pakistan. With hands-on experience in Frappe and ERPNext,
+                        I specialize in crafting efficient, user-friendly business applications
+                        that automate workflows and solve real-world problems.
+                    </div>
+                    <div className="px-[20px] text-[14px] w-[50%]">I’m also proficient in modern web development using React,
+                        Vue.js, and Python. I enjoy creating clean interfaces, working with APIs,
+                        and building responsive dashboards that help users make smarter decisions.
+                    </div>
+                </div>
+                <div className="pr-[20px] flex pt-[10px] text-[14px]">Beyond coding, I’m passionate about innovation and meaningful problem-solving.
+                    Whether it’s building custom ERP modules or contributing to full-stack projects,
+                    I take pride in delivering polished solutions that add real value. I'm always open to new challenges and excited to collaborate with forward-thinking teams.
+
+                </div>
             </div>
 
         </div>
