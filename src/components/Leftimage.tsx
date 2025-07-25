17aa8c9d34a8a6851e0ea2e6e550688df112d30c
Ahsan Javaid <124583633+ehsanjavaid@users.noreply.github.com>
2025-07-25 12:53:11 +0500

    left image change

--- src/components/Leftimage.tsx
+++ src/components/Leftimage.tsx
@@ -1,7 +1,7 @@
 const LeftImage = () => {
   return (
     <div className="w-1/2 fixed top-0 left-0 bg-gray-200 z-0">
-      <img src="/ahsanjavaid.jpg" alt="Profile" className=" max-h-[80%]" />
+      <img src="/myimage.jpeg" alt="Profile" className=" max-h-[80%]" />
     </div>
   );
 };
