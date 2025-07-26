6544b5b1c0b4a6891e20e1389b5563de67e4051e
Ahsan Javaid <124583633+ehsanjavaid@users.noreply.github.com>
2025-07-26 15:51:18 +0500

    left image color change

--- src/components/Leftimage.tsx
+++ src/components/Leftimage.tsx
@@ -1,8 +1,16 @@
 const LeftImage = () => {
   return (
     <div className="w-1/2 fixed top-0 left-0 bg-gray-200 z-0">
-      <img src="/myimage.jpeg" alt="Profile" className=" max-h-[80%]" />
+      <div className="relative h-screen">
+        <img
+          src="/myimage.jpeg"
+          alt="Profile"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-[#6CE4D3] opacity-40 mix-blend-multiply"></div>
+      </div>
     </div>
+
   );
 };
 
